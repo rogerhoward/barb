5fbab1382f51d4558636d5292b8787c59bbe1ac1
Roger Howard <rogerhoward@gmail.com>
2016-02-16 23:09:28 -0800

    Added slack_log() function

--- hook.py
+++ hook.py
@@ -7,13 +7,31 @@ import rethinkdb as r
 app = Flask(__name__)
 
 
+def slack_log(name, request):
+
+    message = {}
+    message['token'] = request.form['token']
+    message['team_id'] = request.form['team_id']
+    message['team_domain'] = request.form['team_domain']
+    message['channel_id'] = request.form['channel_id']
+    message['channel_name'] = request.form['channel_name']
+    message['timestamp'] = request.form['timestamp']
+    message['user_id'] = request.form['user_id']
+    message['user_name'] = request.form['user_name']
+    message['text'] = request.form['text']
+    message['trigger_word'] = request.form['trigger_word']
+
+    response = r.db("hookdb").table(name).insert(message, conflict ="update").run()
+
+    print(request.form['text'])
+
 # Basic hook handler
 @app.route('/slack/<name>', methods=['GET', 'POST'])
 def hook(name):
-if request.method == 'POST':
-        do_the_login()
-    else:
-        show_the_login_form()
+    if request.method == 'POST':
+            slack_log(name, request)
+        else:
+            print('Sorry, {} is not supported by this endpoint'.format(request.method))
 
 
 @app.after_request
@@ -26,4 +44,5 @@ def add_header(response):
     return response
 
 if __name__ == '__main__':
+    r.connect("localhost", 28015).repl()
     app.run(processes=3, host='0.0.0.0')
