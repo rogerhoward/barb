de1e080b429fbf702e11a9388842be741caa456d
Roger Howard <rogerhoward@gmail.com>
2016-02-25 00:12:07 -0800

    Added bot handler

--- hook.py
+++ hook.py
@@ -37,6 +37,28 @@ def hook(name):
     if log: print('hook({})'.format(name))
     return jsonify(slack_log(name, request))
 
+
+# ping handler
+# token=cMj9u7CkpemtLDTmVd0lkyrQ
+# team_id=T0001
+# team_domain=example
+# channel_id=C2147483705
+# channel_name=test
+# timestamp=1355517523.000005
+# user_id=U2147483697
+# user_name=Steve
+# text=googlebot: What is the air-speed velocity of an unladen swallow?
+# trigger_word=googlebot:
+
+@app.route('/bot', methods=['POST'])
+def bot():
+    if log: print('bot()')
+    user_name = request.form['user_name']
+    text = request.form['text']
+
+    return jsonify({'text':'Received from {}'.format(user_name)})
+
+
 # Basic root handler
 @app.route('/', methods=['GET'])
 def root():
