6c52c9d6f467a37fd5f32c9873ae71b43ba81d49
Roger Howard <rogerhoward@gmail.com>
2016-02-28 15:33:29 -0800

    ping plugin now looks for trigger text anywhere in message

--- plugins/ping/__init__.py
+++ plugins/ping/__init__.py
@@ -3,7 +3,7 @@ import config
 # Ping
 def consider(message):
     if config.log: print('ping considered')
-    if message['text'].startswith('ping'):
+    if 'ping' in message['text']:
         if config.log: print('ping triggered')
         return message
     else:
