76f05091e77021a27219672a439d7e2fe5168b6f
Roger Howard <rogerhoward@gmail.com>
2016-02-28 15:46:04 -0800

    Reenabled original ping behavior

--- plugins/ping/__init__.py
+++ plugins/ping/__init__.py
@@ -1,19 +1,19 @@
 import config
 
-# Ping
-def consider(message):
-    if config.log: print('ping considered')
-    if message['text'].endswith('ping'):
-        if config.log: print('ping triggered')
-        return 'pong'
-    else:
-        return False
-
 # # Ping
 # def consider(message):
 #     if config.log: print('ping considered')
 #     if message['text'].endswith('ping'):
 #         if config.log: print('ping triggered')
-#         return message
+#         return 'pong'
 #     else:
 #         return False
+
+# Ping
+def consider(message):
+    if config.log: print('ping considered')
+    if message['text'].endswith('ping'):
+        if config.log: print('ping triggered')
+        return message
+    else:
+        return False
