cb2d347c8accc3cbf44f119646e4e3d5777009f3
Roger Howard <rogerhoward@gmail.com>
2016-02-28 15:44:00 -0800

    Changed ping behavior

--- plugins/ping/__init__.py
+++ plugins/ping/__init__.py
@@ -5,6 +5,15 @@ def consider(message):
     if config.log: print('ping considered')
     if message['text'].endswith('ping'):
         if config.log: print('ping triggered')
-        return message
+        return 'pong'
     else:
         return False
+
+# # Ping
+# def consider(message):
+#     if config.log: print('ping considered')
+#     if message['text'].endswith('ping'):
+#         if config.log: print('ping triggered')
+#         return message
+#     else:
+#         return False
