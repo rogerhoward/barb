1a1ffd19fd1f64497163edf168c4852093567a90
Roger Howard <rogerhoward@gmail.com>
2016-02-28 08:29:53 -0800

    Changed plugin_directory to absolute path

--- config.py
+++ config.py
@@ -4,7 +4,13 @@ Load by 'import config', not 'from config import *'
 Access properties as 'config.property'
 """
 
-plugin_directory = "./plugins"
+import os
+
+project_directory = os.path.dirname(os.path.realpath(__file__))
+
+plugin_directory_name = "plugins"
+plugin_directory = os.path.join(project_directory, plugin_directory_name)
+
 main_entry_point = "__init__"
 
 tokens = ['']
