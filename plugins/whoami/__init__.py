e9d66fb06038ca7e7719321c3f3dcb704a852635
Roger Howard <rogerhoward@gmail.com>
2016-02-28 23:46:34 -0800

    change whoami return format

--- plugins/whoami/__init__.py
+++ plugins/whoami/__init__.py
@@ -10,6 +10,6 @@ def consider(message):
     if config.log: print('whoami considered')
     if 'whoami' in message['text']:
         if config.log: print('whoami triggered')
-        return message['user_name']
+        return 'you are {}'.format(message['user_name'])
     else:
         return False
