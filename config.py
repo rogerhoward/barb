2bcf7be5f0085173ec644d5d09b84414a06a2c42
Roger Howard <rogerhoward@gmail.com>
2016-03-01 22:20:11 -0800

    Cleaned up config

--- config.py
+++ config.py
@@ -8,10 +8,8 @@ import os, json
 
 project_directory = os.path.dirname(os.path.realpath(__file__))
 
-plugin_directory_name = "plugins"
+plugin_directory_name = 'plugins'
 plugin_directory = os.path.join(project_directory, plugin_directory_name)
-
-main_entry_point = "__init__"
 
 tokens = ['']
 db_name = 'hookdb'
