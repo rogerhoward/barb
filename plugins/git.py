325dac145a12ac01eef4d6ef2de68ef027a2c7b2
Roger Howard <rogerhoward@gmail.com>
2016-07-06 18:49:25 -0700

    Cleanup commit

---
+++ plugins/git.py
@@ -0,0 +1,18 @@
+import config
+import git 
+
+# Git commands
+def consider(message):
+    """Git: attempts to git pull the current project.
+
+    Return: Status message, or False.
+    """
+    if config.log: print('git pull considered')
+    possible_trigger = '{} git pull'.format(message['trigger_word'])
+    if message['text'].startswith(possible_trigger):
+        if config.log: print('git pull triggered')
+        g = git.cmd.Git(config.project_directory)
+        g.pull()
+        return 'git pulled'
+    else:
+        return False
